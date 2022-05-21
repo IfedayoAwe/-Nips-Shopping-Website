6446fc6c3646088ee6ff611b50aa076e479b3941
IfedayoAwe <olalekanawe99@gmail.com>
2022-05-21 16:04:33 +0100

    templates, static files

---
+++ store/urls.py
@@ -0,0 +1,9 @@
+from unicodedata import name
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.store, name='store'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+]
