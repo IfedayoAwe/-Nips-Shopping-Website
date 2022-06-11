1023c2bbe5731d7efeaeb4ac4ce3ad009955e87e
IfedayoAwe <olalekanawe99@gmail.com>
2022-06-11 10:57:16 +0100

    get cart total, get num of cart items, upate cart item

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Nips-Shopping-Website
 
 ## Description
-This repository contains the the code that each file in the project directory of building an ecommerce website with a full registration and authentication system.
+This repository contains the the code that each file in the project directory of building an ecommerce website with a full registration and authentication system. The website is assicible by authenticated and unauthenticated users for those who wants to be customers and those who simply want to make a purchase and do not want to have to register. 
--- static/js/cart.js
+++ static/js/cart.js
@@ -4,9 +4,6 @@ for (i = 0; i < updateBtns.length; i++) {
 	updateBtns[i].addEventListener('click', function(){
 		var productId = this.dataset.product
 		var action = this.dataset.action
-		console.log('productId:', productId, 'Action:', action)
-		console.log('USER:', user)
-
 		if (user == 'AnonymousUser'){
 			addCookieItem(productId, action)
 		}else{
@@ -16,7 +13,6 @@ for (i = 0; i < updateBtns.length; i++) {
 }
 
 function updateUserOrder(productId, action){
-	console.log('User is authenticated, sending data...')
 
 		var url = '/update_item/'
 
@@ -29,15 +25,14 @@ function updateUserOrder(productId, action){
 			body:JSON.stringify({'productId':productId, 'action':action})
 		})
 		.then((response) => {
-		   return response.json();
+		   return response.json()
 		})
 		.then((data) => {
 		    location.reload()
-		});
+		})
 }
 
 function addCookieItem(productId, action){
-	console.log('User is not authenticated')
 
 	if (action == 'add'){
 		if (cart[productId] == undefined){
@@ -52,11 +47,9 @@ function addCookieItem(productId, action){
 		cart[productId]['quantity'] -= 1
 
 		if (cart[productId]['quantity'] <= 0){
-			console.log('Item should be deleted')
 			delete cart[productId];
 		}
 	}
-	console.log('CART:', cart)
 	document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
 	
 	location.reload()
